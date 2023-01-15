4e2d239b0c450b46188c0db977df7504dbd72a0d
ernis0.0 <nhtfxnhtfx.9@gmail.com>
2023-01-15 20:43:34 +0300

    first commit

---
+++ pages/Loading/Loading.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import React, { FC } from "react";
+import s from "../Loading/style.module.scss";
+import { AiOutlineClose } from "react-icons/ai";
+
+interface Person {
+  onClick: () => void;
+}
+const Loading: FC<Person> = ({ onClick }) => {
+  return (
+    <div className={s.header}>
+      <div className={s.block}>
+        <Image
+          src="/ernis.jpg"
+          
+          className={s.image}
+          layout="fill"
+          
+          alt="/"
+        />
+        
+          <div className={s.info}>
+       <p>
+       Добро пожаловать Меня зовут Эрнис  я фронтенд разработчик  <em/>Здесь вы можете увидеть мои проекты
+       </p>
+         
+       </div>
+       <AiOutlineClose  onClick={onClick} className={s.AiOutlineClose} />
+        <div>
+       
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
